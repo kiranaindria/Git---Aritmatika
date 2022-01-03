0b0ad32e18e0cb6bf41c51d0a55eda416b59d95c
Kirana Indria <kirana.indria@mhs.itenas.ac.id>
2022-01-03 08:23:20 +0700

    Menambahkan fungsionalitas add dan sub

---
+++ src/com/oop/git/Aritmatika.java
@@ -0,0 +1,18 @@
+package com.oop.git;
+
+public class Aritmatika {
+	// Developer 1
+	public double add(int a, int b) {
+		return a + b;
+	}
+	
+	// Developer 1
+	public double sub(int a, int b) {
+		return a - b;
+	}
+	
+	// Developer 2 (mult)
+	
+	// Developer 3 (div)
+	
+}
